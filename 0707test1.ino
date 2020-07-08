90e5b73e9b905569df8dc163fcf7aed5dde6ab92
MartianSheep <mkmarsscience@gmail.com>
2020-07-08 16:44:26 +0800

    reference old version code (Caution: Dirty)

---
+++ 0707test1.ino
@@ -0,0 +1,316 @@
+/*     Simple Stepper Motor Control Exaple Code
+ *      
+ *  by Dejan Nedelkovski, www.HowToMechatronics.com
+ *  
+ */
+ #include <Stepper.h>
+ #include <Servo.h>
+ #define LINE_BUFFER_LENGTH 512
+// defines pins numbers
+/*const int stepPin1 = 3; 
+const int dirPin1 = 4; 
+const int stepPin2 = 5; 
+const int dirPin2 = 6; */
+const int stepsPerRevolution = 200; 
+const int penZUp = 90;
+const int penZDown = 60;
+int pos = 0;
+const int penServoPin = 7;
+
+Stepper myStepperX(stepsPerRevolution, 3, 4);
+Stepper myStepperY(stepsPerRevolution, 5, 6);
+Servo penServo;
+
+struct point { 
+  float x; 
+  float y; 
+  float z; 
+};
+
+struct point actuatorPos;
+
+float StepInc = 1;
+int StepDelay = 0;
+int LineDelay = 50;
+int penDelay = 50;
+
+float StepsPerMillimeterX = 6.0;
+float StepsPerMillimeterY = 6.0;
+
+float Xmin = 0;
+float Xmax = 100;
+float Ymin = 0;
+float Ymax = 100;
+float Zmin = 0;
+float Zmax = 1;
+
+float Xpos = Xmin;
+float Ypos = Ymin;
+float Zpos = Zmax; 
+
+boolean verbose = false;
+
+ 
+void setup() {
+  // Sets the two pins as Outputs
+  Serial.begin( 9600 );
+  penServo.attach(penServoPin);
+  penServo.write(penZUp);
+  delay(200);
+  myStepperX.setSpeed(50);
+  myStepperY.setSpeed(50); 
+  
+}
+void loop() {
+  /*myStepperX.step(-1000);
+  delayMicroseconds(5000); 
+  myStepperX.step(1000);
+  delayMicroseconds(5000);
+  
+  for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
+    // in steps of 1 degree
+    penServo.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(15);                       // waits 15ms for the servo to reach the position
+  }
+  for (pos = 180; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
+    penServo.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(15);                       // waits 15ms for the servo to reach the position
+  }*/
+  
+  // delay(200);
+  char line[ LINE_BUFFER_LENGTH ];
+  char c;
+  int lineIndex;
+
+  while ( Serial.available()>0 ) {
+      c = Serial.read();
+      Serial.println();
+      line[ lineIndex++ ] = c;
+      for(int i=0; i<lineIndex; i++)
+      {
+        Serial.print(line[i]);
+      }
+      
+  }
+  processIncomingLine( line, lineIndex );
+}
+
+void processIncomingLine( char* line, int charNB ) {
+  int currentIndex = 0;
+  char buffer[ 64 ];                                 // Hope that 64 is enough for 1 parameter
+  struct point newPos;
+
+  newPos.x = 0.0;
+  newPos.y = 0.0;
+
+  //  Needs to interpret 
+  //  G1 for moving
+  //  G4 P300 (wait 150ms)
+  //  G1 X60 Y30
+  //  G1 X30 Y50
+  //  M300 S30 (pen down)
+  //  M300 S50 (pen up)
+  //  Discard anything with a (
+  //  Discard any other command!
+
+  while( currentIndex < charNB ) {
+    switch ( line[ currentIndex++ ] ) {              // Select command, if any
+    case 'U':
+      penUp(); 
+      break;
+    case 'D':
+      penDown(); 
+      break;
+    case 'G':
+      buffer[0] = line[ currentIndex++ ];          // /!\ Dirty - Only works with 2 digit commands
+      //      buffer[1] = line[ currentIndex++ ];
+      //      buffer[2] = '\0';
+      buffer[1] = '\0';
+
+      switch ( atoi( buffer ) ){                   // Select G command
+      case 0:                                   // G00 & G01 - Movement or fast movement. Same here
+      case 1:
+        // /!\ Dirty - Suppose that X is before Y
+        char* indexX = strchr( line+currentIndex, 'X' );  // Get X/Y position in the string (if any)
+        char* indexY = strchr( line+currentIndex, 'Y' );
+        if ( indexY <= 0 ) {
+          newPos.x = atof( indexX + 1); 
+          newPos.y = actuatorPos.y;
+        } 
+        else if ( indexX <= 0 ) {
+          newPos.y = atof( indexY + 1);
+          newPos.x = actuatorPos.x;
+        } 
+        else {
+          newPos.y = atof( indexY + 1);
+          indexY = '\0';
+          newPos.x = atof( indexX + 1);
+        }
+        drawLine(newPos.x, newPos.y );
+        //        Serial.println("ok");
+        actuatorPos.x = newPos.x;
+        actuatorPos.y = newPos.y;
+        break;
+      }
+      break;
+    case 'M':
+      buffer[0] = line[ currentIndex++ ];        // /!\ Dirty - Only works with 3 digit commands
+      buffer[1] = line[ currentIndex++ ];
+      buffer[2] = line[ currentIndex++ ];
+      buffer[3] = '\0';
+      switch ( atoi( buffer ) ){
+      case 300:
+        {
+          char* indexS = strchr( line+currentIndex, 'S' );
+          float Spos = atof( indexS + 1);
+          //          Serial.println("ok");
+          if (Spos == 30) { 
+            penDown(); 
+          }
+          if (Spos == 50) { 
+            penUp(); 
+          }
+          break;
+        }
+      case 114:                                // M114 - Repport position
+        Serial.print( "Absolute position : X = " );
+        Serial.print( actuatorPos.x );
+        Serial.print( "  -  Y = " );
+        Serial.println( actuatorPos.y );
+        break;
+      default:
+        Serial.print( "Command not recognized : M");
+        Serial.println( buffer );
+      }
+    }
+  }
+
+
+
+}
+
+
+
+void drawLine(float x1, float y1) {
+
+  if (verbose)
+  {
+    Serial.print("fx1, fy1: ");
+    Serial.print(x1);
+    Serial.print(",");
+    Serial.print(y1);
+    Serial.println("");
+  }  
+
+  
+  if (x1 >= Xmax) { 
+    x1 = Xmax; 
+  }
+  if (x1 <= Xmin) { 
+    x1 = Xmin; 
+  }
+  if (y1 >= Ymax) { 
+    y1 = Ymax; 
+  }
+  if (y1 <= Ymin) { 
+    y1 = Ymin; 
+  }
+
+  if (verbose)
+  {
+    Serial.print("Xpos, Ypos: ");
+    Serial.print(Xpos);
+    Serial.print(",");
+    Serial.print(Ypos);
+    Serial.println("");
+  }
+
+  if (verbose)
+  {
+    Serial.print("x1, y1: ");
+    Serial.print(x1);
+    Serial.print(",");
+    Serial.print(y1);
+    Serial.println("");
+  }
+
+  //  Convert coordinates to steps
+  x1 = (int)(x1*StepsPerMillimeterX);
+  y1 = (int)(y1*StepsPerMillimeterY);
+  float x0 = Xpos;
+  float y0 = Ypos;
+
+  //  Let's find out the change for the coordinates
+  long dx = abs(x1-x0);
+  long dy = abs(y1-y0);
+  int sx = x0<x1 ? StepInc : -StepInc;
+  int sy = y0<y1 ? StepInc : -StepInc;
+
+  long i;
+  long over = 0;
+
+  if (dx > dy) {
+    for (i=0; i<dx; ++i) {
+      myStepperX.step(sx);
+      over+=dy;
+      if (over>=dx) {
+        over-=dx;
+        myStepperY.step(sy);
+      }
+      delay(StepDelay);
+    }
+  }
+  else {
+    for (i=0; i<dy; ++i) {
+      myStepperY.step(sy);
+      over+=dx;
+      if (over>=dy) {
+        over-=dy;
+        myStepperX.step(sx);
+      }
+      delay(StepDelay);
+    }    
+  }
+
+  if (verbose)
+  {
+    Serial.print("dx, dy:");
+    Serial.print(dx);
+    Serial.print(",");
+    Serial.print(dy);
+    Serial.println("");
+  }
+
+  if (verbose)
+  {
+    Serial.print("Going to (");
+    Serial.print(x0);
+    Serial.print(",");
+    Serial.print(y0);
+    Serial.println(")");
+  }
+
+  
+  delay(LineDelay);
+  
+  Xpos = x1;
+  Ypos = y1;
+}
+
+
+void penUp() { 
+  penServo.write(penZUp); 
+  delay(LineDelay); 
+  Zpos=Zmax; 
+  if (verbose) { 
+    Serial.println("Pen up!"); 
+  } 
+}
+void penDown() { 
+  penServo.write(penZDown); 
+  delay(LineDelay); 
+  Zpos=Zmin; 
+  if (verbose) { 
+    Serial.println("Pen down."); 
+  } 
+}
