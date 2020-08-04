ce4a6e2906a7d8465ab39b867ef612b1efe70e94
MartianSheep <mkmarsscience@gmail.com>
2020-08-04 21:19:23 +0800

    20 degree

--- Elevate_Gcode_teaching/configuration.h
+++ Elevate_Gcode_teaching/configuration.h
@@ -62,7 +62,7 @@
 
 /**************** Servo Motor start ****************/
 	#define Servo_Pin				9
-	#define Servo_Pen_Up_Angle		15
+	#define Servo_Pen_Up_Angle		20
 	#define Servo_Pen_Limit_Angle	30
 
 	#define Pen_Delay_Time			100
